466cb4a9542928f021a6866bd4797b6055b257cb
Maayan <d46880@ganyavne.ort.org.il>
2023-09-27 17:06:06 +0300

    creating gripper system

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,12 +2,18 @@ package frc.robot;
 
 import com.flash3388.flashlib.frc.robot.FrcRobotControl;
 import com.flash3388.flashlib.frc.robot.base.iterative.IterativeFrcRobot;
+import com.flash3388.flashlib.hid.XboxAxis;
+import com.flash3388.flashlib.hid.XboxController;
 import com.flash3388.flashlib.robot.base.DelegatingRobotControl;
+import frc.robot.subSystems.Gripper;
 
 public class Robot extends DelegatingRobotControl implements IterativeFrcRobot {
-
+    private Gripper gripper;
+    private XboxController xbox;
     public Robot(FrcRobotControl robotControl) {
         super(robotControl);
+        this.gripper = new Gripper();
+        this.xbox = getHidInterface().newXboxController(RobotMap.XBOX);
     }
 
     @Override
@@ -27,7 +33,7 @@ public class Robot extends DelegatingRobotControl implements IterativeFrcRobot {
 
     @Override
     public void teleopPeriodic() {
-
+        gripper.move(xbox.getAxis(XboxAxis.LeftStickY).getAsDouble());
     }
 
     @Override
---
+++ src/main/java/frc/robot/subSystems/Gripper.java
@@ -0,0 +1,29 @@
+package frc.robot.subSystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.flash3388.flashlib.scheduling.Subsystem;
+import frc.robot.RobotMap;
+
+public class Gripper extends Subsystem {
+    private WPI_TalonSRX talonSRX;
+    public Gripper() {
+        this.talonSRX= new WPI_TalonSRX(RobotMap.GRIPPER);
+    }
+    public void holdCone() {
+        talonSRX.set(0.2);
+    }
+    public void pickCone() {
+        talonSRX.set(2);
+    }
+    public void releaseCone(){
+        talonSRX.set(-2);
+    }
+
+    public void move(double speed){
+        talonSRX.set(speed);
+    }
+    public void stop(){
+        talonSRX.set(0);
+    }
+
+}
